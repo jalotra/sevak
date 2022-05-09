26b65e7515c6b23e76d4be0892a3d82ff73230d1
jalotra <shivam.jalotra@flipkart.com>
2022-05-10 02:43:59 +0530

    Will have a config.init file here.

---
+++ sevak/configs/config.py
@@ -0,0 +1,10 @@
+
+import os
+
+# Returns this folder path : /users/jalotra/XYZ
+def get_abs_path():
+    return os.path.dirname(os.path.abspath(__file__))
+
+# Returns config file path : /users/jalotra/XYZ/config.ini
+def get_config_file_path(): 
+    return os.paht.join(get_abs_path(), "config.ini")
